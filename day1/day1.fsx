24c02bab59ef1a59d51bd35a3a6426a22c9d0b0a
Teodor de Fine Licht <teodor@musicmedia.dk>
2024-12-12 17:45:15 +0100

    Update day1

--- day1/day1.fsx
+++ day1/day1.fsx
@@ -2,7 +2,6 @@ open System
 
 let input = IO.File.ReadAllLines "input.txt"
 
-// part one 
 let trimmedInput = input |> Array.map (fun entry -> entry.Split (" ", StringSplitOptions.RemoveEmptyEntries))
 let intInput = trimmedInput |> Array.map (fun a -> (int a[0], int a[1]))
 
@@ -10,22 +9,20 @@ let firstColumn = intInput |> Array.map (fun p -> fst p)
 let secondColumn = intInput |> Array.map (fun p -> snd p)
 
 
-// let sortedFirstColumn = firstColumn |> Array.sort
-// let sortedSecondColumn = secondColumn |> Array.sort
+// PART 1 
+let sortedFirstColumn = firstColumn |> Array.sort
+let sortedSecondColumn = secondColumn |> Array.sort
 
-// let mutable i = 0
-// let mutable diff = 0
-// while i <= firstColumn.Length-1 do
-   //  diff <- diff + abs (sortedFirstColumn[i] - sortedSecondColumn[i])
-//     i <- i + 1
+let mutable i = 0
+let mutable diff = 0
+while i <= firstColumn.Length-1 do
+    diff <- diff + abs (sortedFirstColumn[i] - sortedSecondColumn[i])
+    i <- i + 1
 
-// printfn "sorted first column: %A" sortedFirstColumn
-// printfn "sorted second column: %A" sortedSecondColumn
-
-// printfn "total diff: %A" diff
+printfn "Total distanceb between lists: %A" diff
 
 
-// part two
+// PART 2
 
 let mutable totalSimilarityScore = 0
 for i in firstColumn do
@@ -35,7 +32,8 @@ for i in firstColumn do
             iFreq <- iFreq + 1
     totalSimilarityScore <- totalSimilarityScore + (i * iFreq)
 
-printfn "total similarity score: %A" totalSimilarityScore
+printfn "Total similarity score: %A" totalSimilarityScore
 
-
+// VERY imperative, might as well have written it in Python
+// Maybe try rewriting to a functional version some day
 
