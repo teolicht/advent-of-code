df6a86e7e4526165dc49af47d24f88477773c0fe
Teodor de Fine Licht <teodor@musicmedia.dk>
2024-12-21 17:07:04 +0100

    Finish day4

--- day4/day4.fsx
+++ day4/day4.fsx
@@ -23,37 +23,63 @@ let columns (input: string array) : string list =
 
 // Returns number of matches for "XMAS" and backwards
 let matchXmas (input: string list) : int =
-    input |> List.fold (fun s str -> s + Regex.Count(str, "XMAS|SAMX")) 0
+    let xmas (str: string) : int =
+        let a = Regex.Count(str, "XMAS")
+        let b = Regex.Count(str, "SAMX")
+        a + b
+        
+    input |> List.fold (fun s str -> s + xmas(str)) 0
 
 // Given a specific row t, start finding the diagonal from (t,0), in southeast direction
 // (t, 0) -> (t+1, 1) -> (t+2, 2) -> ... -> (139, 139)
-let diagonalSE (t: int) (n: int) (arr: char array2d) : string =
+let diagonalSE1 (t: int) (n: int) (arr: char array2d) : string =
     [t .. n] |> List.mapi (fun i x -> arr[x,i]) |> charsToString
 
-// Given a specific row t, start finding the diagonal from (t,0), in northeast direction
-// (t, 0) -> (t-1, 1) -> (t-2, 2) -> ... -> (0, 139)
-let diagonalNE (t: int) (n: int) (arr: char array2d) : string =
-    [t .. n] |> List.mapi (fun i _ -> arr[n-t-i,i]) |> charsToString
+// Given a specific col t, start finding the diagonal from (0, t), in southeast direction
+// (0, t) -> (1, t+1) -> (2, t+2) -> ... -> (139-t, 139)
+let diagonalSE2 (t: int) (n: int) (arr: char array2d) : string =
+    [t .. n] |> List.mapi (fun i x -> arr[i,x]) |> charsToString
 
-// Given a specific row t, start finding the diagonal from (t,139), in southwest direction
-// (t, 139) -> (t+1, 138) -> (t+2, 137) -> ... (139, 0)
-let diagonalSW (t: int) (n: int) (arr: char array2d) : string =
-    [t .. n] |> List.mapi (fun i _ -> arr[i,n-t-i]) |> charsToString
+// Given a specific row t, start finding the diagonal from (t, 139) in southwest direction
+// (t, 139) -> (t+1, 138) -> (t+2, 137) -> ... -> (139, 139-t)
+let diagonalSW1 (t: int) (n: int) (arr: char array2d) : string =
+    [t .. n] |> List.mapi (fun i _ -> arr[t+i,n-i]) |> charsToString
 
-// Given a specific row t, start finding the diagonal from (t,139), in northwest direction 
-// (t, 139) -> (t-1, 138) -> (t-2, 137) -> ... (0, 0)
-let diagonalNW (t: int) (n: int) (arr: char array2d) : string =
-    [t .. n] |> List.mapi (fun i _ -> arr[n-t-i,n-t-i]) |> charsToString
+// Given a specific col t, start finding the diagonal from (0, t) in southwest direction
+// (0, t) -> (1, t-1) -> (2, t-2) -> ... -> (139-t, 0)
+let diagonalSW2 (t: int) (arr: char array2d) : string =
+    [0 .. t] |> List.mapi (fun i _ -> arr[i,t-i]) |> charsToString
 
 // Gather all diagonals
 let diagonals (arr: char array2d) =
     let n = arr[0,*].Length-1
-    let seDiagonals = [0 .. n] |> List.map (fun t -> diagonalSE t n arr);
-    let neDiagonals = [0 .. n] |> List.map (fun t -> diagonalNE t n arr);
-    let swDiagonals = [0 .. n] |> List.map (fun t -> diagonalSW t n arr);
-    let nwDiagonals = [0 .. n] |> List.map (fun t -> diagonalNW t n arr)
-    seDiagonals @ neDiagonals @ swDiagonals @ nwDiagonals
-    
+    let se1Diagonals = [0 .. n] |> List.map (fun t -> diagonalSE1 t n arr)
+    let se2Diagonals = [1 .. n] |> List.map (fun t -> diagonalSE2 t n arr)
+    let sw1Diagonals = [0 .. n] |> List.map (fun t -> diagonalSW1 t n arr)
+    let sw2Diagonals = [0 .. n-1] |> List.map (fun t -> diagonalSW2 t arr)
+    se1Diagonals @ se2Diagonals @ sw1Diagonals @ sw2Diagonals
+
+// Given a specific position in the matrix, determine if it is the center of two crossed
+// 'MAS' sequences (which may be in reversed order)
+let isCrossMAS (i: int, j: int) (arr: char array2d) : int =
+    let mas (str: string) =
+        if Regex.IsMatch(str, "MAS") || Regex.IsMatch(str, "SAM") then true
+        else false
+    // Southeast diagonal
+    let diag1 = [ arr[i-1,j-1]; arr[i,j]; arr[i+1,j+1] ] |> charsToString
+    // Southwest diagonal
+    let diag2 = [ arr[i-1,j+1]; arr[i,j]; arr[i+1,j-1] ] |> charsToString
+    if mas diag1 && mas diag2 then 1
+    else 0
+
+// Count number of crossed 'MAS'
+let countCrossMAS (arr: char array2d) =
+    let mutable amount = 0
+    for i in [1 .. arr[*,0].Length-2] do 
+        for j in [1 .. arr[i,*].Length-2] do
+            if arr[i,j] = 'A' then
+                amount <- amount + isCrossMAS (i, j) arr
+    amount
 
 let input = readInput "input.txt"
 let table = input |> Array.toList |> List.map Seq.toList |> array2D 
@@ -61,9 +87,7 @@ let table = input |> Array.toList |> List.map Seq.toList |> array2D
 let diagonalMatches = diagonals table |> matchXmas 
 let horizontalMatches = input |> rows |> matchXmas
 let verticalMatches = input |> columns |> matchXmas
-
 let totalMatches = diagonalMatches + horizontalMatches + verticalMatches
-printfn "%A" totalMatches
 
-
-// PROBLEM: bounds of the diagonals, and also maybe have to start from column and not row
+printfn "Total 'XMAS' matches: %A" totalMatches
+printfn "Total cross-'MAS' matches: %A" (countCrossMAS table)
